2cb4e54f27ad138e61e5e1a18cdb148bb9e961ca
Sevki Hasirci <s@sevki.org>
2015-04-05 04:45:35 +0300

    initial commit of the prom exporter package

--- package.js
+++ package.js
@@ -1,11 +1,14 @@
 Package.describe({
   summary: "Returns diagnostics info on server internals via a connect route.",
   version: "0.2.2",
-  name: "percolate:server-info",
+  name: "sevki:prometheus-exporter",
   git: "https://github.com/percolatestudio/meteor-server-info"
 });
 
-Npm.depends({connect: "2.9.0"});
+Npm.depends({
+  "connect": "2.9.0",
+  "sprintf-js": "1.0.2"
+});
 
 Package.on_use(function (api, where) {
   api.versionsFrom('METEOR@1.0.1');
