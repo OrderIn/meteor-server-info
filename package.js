df92e974ad82906365e2a600cb4696ff9f8ba610
Sevki Hasirci <s@sevki.org>
2015-04-06 01:39:14 +0300

    package functions

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: "Returns diagnostics info on server internals via a connect route.",
   version: "0.2.2",
   name: "sevki:prometheus-exporter",
-  git: "https://github.com/percolatestudio/meteor-server-info"
+  git: "https://github.com/percolatestudio/meteor-server-info.git"
 });
 
 Npm.depends({
@@ -10,10 +10,10 @@ Npm.depends({
   "sprintf-js": "1.0.2"
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
   api.versionsFrom('METEOR@1.0.1');
   api.use(['webapp', 'mongo-livedata', 'facts'], 'server');
-  api.add_files('server-info.js', 'server');
+  api.addFiles('server-info.js', 'server');
 
   if (api.export)
     api.export('ServerInfo', 'server');
